ed454fdcc2ffa9a277fccfe624ded65a156308d8
jaKa <jaka@koofr.net>
2018-12-17 13:14:25 +0100

    Use recent search.

--- lib/twitter_ebooks/bot.rb
+++ lib/twitter_ebooks/bot.rb
@@ -353,7 +353,7 @@ module Ebooks
           two_days_ago = Time.now - 2
           since = two_days_ago.strftime("%Y-%m-%d")
           log "searching for 'to:#{username} since:#{since}'"
-          twitter.search("to:#{username} since:#{since}")
+          twitter.search("to:#{username} since:#{since}", result_type: "recent")
             .select { |t| t.created_at > last }
             .sort { |t1,t2| t2.created_at <=> t1.created_at }
             .each do |tweet|
